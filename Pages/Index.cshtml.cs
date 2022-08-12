7f0feca40f199f860eac8e03131c568b09ae5140
vit-krizka <vit.krizka@gmail.com>
2022-08-12 12:09:09 +0200

    homepage

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using contact_manager.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace contact_manager.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly contact_manager.Data.ContactContext _context;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, contact_manager.Data.ContactContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public void OnGet()
-    {
+    public IList<Contact> Contact { get; set; } = default!;
 
+    //public void OnGet()
+    //{
+
+    //}
+
+    public async Task OnGetAsync()
+    {
+        if (_context.Contact != null)
+        {
+            Contact = await _context.Contact.ToListAsync();
+        }
     }
 }
+
+
+
 
